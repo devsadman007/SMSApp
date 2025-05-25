d1781ce64520cb1b39155f3a15169b3565aa8267
devsadman007 <User@DESKTOP-H6MDSIP>
2025-05-25 09:03:41 +0100

    Basic Crud

--- SMSApp/wwwroot/js/site.js
+++ SMSApp/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0])
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
